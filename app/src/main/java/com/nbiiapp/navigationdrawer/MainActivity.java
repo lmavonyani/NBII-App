5d4525420c0c23bc1fba5244187e9b01b7d4e92f
Beaton Nyamapanda <beatnyama@gmail.com>
2017-07-13 16:54:41 +0100

    Additional changes to toast message

--- app/src/main/java/com/nbiiapp/navigationdrawer/MainActivity.java
+++ app/src/main/java/com/nbiiapp/navigationdrawer/MainActivity.java
@@ -36,16 +36,16 @@ public class MainActivity extends AppCompatActivity {
 
                 switch (id){
                     case R.id.home:
-                        Toast.makeText(getApplicationContext(),"Home toast",Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getApplicationContext(),"Home toast updated",Toast.LENGTH_SHORT).show();
                         //let call a fragment here
                         drawerLayout.closeDrawers();
                         break;
                     case R.id.settings:
-                        Toast.makeText(getApplicationContext(),"Settings toast",Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getApplicationContext(),"Settings toast updated",Toast.LENGTH_SHORT).show();
                         //let call a fragment here
                         break;
                     case R.id.trash:
-                        Toast.makeText(getApplicationContext(),"Trash",Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getApplicationContext(),"Trash toast updated",Toast.LENGTH_SHORT).show();
                         drawerLayout.closeDrawers();
                         break;
                     case R.id.logout:
